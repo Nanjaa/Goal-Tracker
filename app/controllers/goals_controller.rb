2f8e699a6bdb0fcefca6c980d17797ed75d65aca
Stephanie <steph.o-91@hotmail.com>
2016-06-12 22:46:23 -0500

    You can now create and edit a goal

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,23 +1,38 @@
 class GoalsController < ApplicationController
+	def index
+		@goals = Goal.all
+	end
+
 	def show
 		@goal = Goal.find(params[:id])
 	end
-	
+
 	def new
+		@goal = Goal.new
+	end
+
+	def edit
+		@goal = Goal.find(params[:id])
 	end
 
 	def create
-		# @goal = Goal.new(params.require(:goal).permit(:title))
-
-		# @goal.save = Goal.new(params[:goal])
-
-		# @goal.save
-		# redirect_to @goal
-
 		@goal = Goal.new(goal_params)
 
-		@goal.save
-		redirect_to @goal
+		if @goal.save
+			redirect_to @goal
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		@goal = Goal.find(params[:id])
+
+		if @goal.update(goal_params)
+			redirect_to @goal
+		else
+			render 'edit'
+		end
 	end
 
 	private
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,2 +1,4 @@
 class Goal < ActiveRecord::Base
+	validates :title,	presence: true,
+						length: { minimum: 3 }
 end
