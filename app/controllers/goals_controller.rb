823c17659af5c7e4f00d0007254ca8d4adf5e732
Stephanie <steph.o-91@hotmail.com>
2016-06-12 22:09:01 -0500

    Started over

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,11 +1,27 @@
 class GoalsController < ApplicationController
+	def show
+		@goal = Goal.find(params[:id])
+	end
+	
 	def new
 	end
 
 	def create
-		@goal = Goal.new(params.require(:goal).permit(:name))
+		# @goal = Goal.new(params.require(:goal).permit(:title))
+
+		# @goal.save = Goal.new(params[:goal])
+
+		# @goal.save
+		# redirect_to @goal
+
+		@goal = Goal.new(goal_params)
 
 		@goal.save
 		redirect_to @goal
 	end
+
+	private
+		def goal_params
+			params.require(:goal).permit(:title)
+		end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608033923) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string   "goal_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20160613024758) do
 
   create_table "goals", force: :cascade do |t|
-    t.string   "name"
+    t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
